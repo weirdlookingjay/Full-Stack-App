c447ab18f2f3dee9d1b853da33e9ee3572f92cb5
Joe Clark <weirdlookingjay@live.com>
2025-05-30 14:06:13 -0400

    Updated homepage UI to list book covers

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+
+const Home = () => (
+  <>
+    <BookOverview {...sampleBooks[0]} />
+
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
+  </>
+);
+export default Home;
--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-const Home = () => (
-  <>
-    <h1 className="text-3xl font-bold underline">Home</h1>
-  </>
-);
-export default Home;
