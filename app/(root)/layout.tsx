16e3f2acaa7558adc3cbc41e0125ec08ddd334ab
Joe Clark <weirdlookingjay@live.com>
2025-06-02 12:35:06 -0400

    Implement qstash workflows for user onboarding

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,39 @@
 import { auth } from "@/auth";
 import Header from "@/components/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 import React, { ReactNode } from "react";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session) redirect("/sign-in");
+
+  //after the page loads
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    // get the user and see if the last activity date is today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0]?.lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({
+        lastActivityDate: new Date().toISOString().slice(0, 10),
+      })
+      .where(eq(users.id, session?.user?.id));
+  });
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import ratelimit from "../ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "../workflow";
+import config from "../config";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
@@ -67,6 +69,14 @@ export const signUp = async (params: AuthCredentials) => {
       password: hasshedPassword,
       universityId,
       universityCard,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
     });
 
     await signInWithCredentials({ email, password });
